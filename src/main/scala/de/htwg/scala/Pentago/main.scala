0da28ed05c0a3538c2bca2af6e0a5841831c500c
Daniel Hipp <d.hipp@gmx.net>
2018-12-20 13:54:50 +0100

    changed main

--- src/main/scala/de/htwg/scala/Pentago/main.scala
+++ src/main/scala/de/htwg/scala/Pentago/main.scala
@@ -1,14 +1,11 @@
-import de.htwg.scala.Pentago.view.{LineReader, PlayerOne, Textview}
+import de.htwg.scala.Pentago.controller.Controller
+import de.htwg.scala.Pentago.view.Textview
 
-object main {
-  def main(args: Array[String]): Unit = {
-    createField()
-  }
+object main extends App {
+  override def main(args: Array[String]): Unit = {
 
-  def createField(): Unit = {
-    //Todo: Create Gamefield over controller and give it to view
-    //Todo: Create while-Loop to play()
-
-    val lineReader = new LineReader()
+    val controller = new Controller("Player1", "Player2")
+    val textview = new Textview()
+    textview.play(controller)
   }
 }
