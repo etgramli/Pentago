2ace0c2269b1afc74212923f2d614f4bca7cdf8a
Daniel Hipp <dahipp@htwg-konstanz.de>
2019-01-07 18:13:20 +0100

    added a lot of stuff to gui, simple functions work

--- src/main/scala/de/htwg/scala/Pentago/main.scala
+++ src/main/scala/de/htwg/scala/Pentago/main.scala
@@ -1,11 +1,15 @@
 import de.htwg.scala.Pentago.controller.Controller
-import de.htwg.scala.Pentago.view.Textview
+import de.htwg.scala.Pentago.view.{GUI, Textview}
 
 object main extends App {
   override def main(args: Array[String]): Unit = {
 
     val controller = new Controller("Player1", "Player2")
     val textview = new Textview()
-    textview.play(controller)
+    val gui = new GUI(controller)
+    gui.visible = true
+    //textview.play(controller)
+
+    println("End of Programm")
   }
 }
